d480bcb0866c3ad6ada6c3e7fd04d24aaeae2e0f
thiagoDMDiniz <thiago.dias.diniz@gmail.com>
2022-07-02 23:09:45 -0300

    final version

--- node/index.js
+++ node/index.js
@@ -10,14 +10,35 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Wesley')`
+const createDatabase = `CREATE TABLE IF NOT EXISTS people(
+    id int not null auto_increment, name VARCHAR(255), primary key(id))`
+  connection.query(createDatabase)
+
+console.log('Created table')
+
+generatedName = 'Thiago' + Math.floor(Math.random() * 1000).toString();
+
+const sql = `INSERT INTO people(name) values('` +generatedName+`')`
 connection.query(sql)
-connection.end()
+//connection.end()
 
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
-})
+app.get('/', (req, res) => {
+    html = '<h1>Full Cycle</h1>'
+  
+    connection.query('SELECT name FROM people', function(err, rows, fields) {
+      if (rows.length > 0) {
+        html += '<div>Names:</div>'
+        html += '<ol>'
+        for(item = 0; item < rows.length; item++) {
+          html += '<li>' + rows[item]['name'] + '</li>'
+        }
+        html += '</ol>'
+      }
+  
+      res.send(html)
+    })
+  })
 
 app.listen(port, ()=> {
     console.log('Rodando na porta ' + port)
